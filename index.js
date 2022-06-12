8c7dc504f7b7ce3be1cb11fa6390c53d35370a32
+dorcus <+wayumulu58@gmail.com>
2022-06-12 16:10:08 +0300

    assignment commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const superbowlWin = (record) => {
+  for (let game of record) {
+    if (game.result === 'W') {
+      return game.year
+    }
+  }
+
+}
+
+// const superbowlWin = (record) => {
+//   return record.year
+// }
+
+// let yearWon = record.find(superbowlWin)
+// console.log(yearWon)
+// record.result === 'W'
